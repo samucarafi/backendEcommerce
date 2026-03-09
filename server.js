93264656d2902c97c35157e49a24d058694fc127
samucarafi <160398348+samucarafi@users.noreply.github.com>
2026-03-09 18:32:39 -0300

    cors

--- server.js
+++ server.js
@@ -9,8 +9,20 @@ export const __filename = fileURLToPath(import.meta.url);
 export const __dirname = dirname(__filename);
 
 export const app = express();
+const allowedOrigins = process.env.FRONTEND_URL.split(",");
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
+app.use(
+  cors({
+    credentials: true,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, origin);
+      } else {
+        callback(new Error("CORS não permitido"));
+      }
+    },
+  }),
+);
 app.use("/tmp", express.static(__dirname + "/tmp"));
 app.use(routes);
